d8ccce76ac60e9d18ce8a75e3c70186d0a6e596d
keerthi <keerthi.bode5@gmailcom>
2017-07-09 12:25:52 +0530

    checking

---
+++ switch/src/Switch.java
@@ -0,0 +1,16 @@
+
+public class Switch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int number=10;  
+		    switch(number){  
+		    case 10: System.out.println("addproducts");  
+		    case 20: System.out.println("listproducts");  
+		    }  
+		} 
+	 }
+		
+
+	
+
